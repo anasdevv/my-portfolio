838d4e903de5a6bd554a9da5674ff492f2dea47f
anasdevv <anas1.dev@gmail.com>
2023-03-12 05:03:28 +0500

    final

--- server.js
+++ server.js
@@ -7,14 +7,17 @@ const app = express();
 const contactRoute = require('./route/contactRoute');
 app.use(express.json());
 app.use(cors());
-app.use('/', contactRoute);
+app.use('/contact', contactRoute);
+// app.use(express.static('client-side/build'));
+const p1 = express.static(path.join(__dirname, 'client-side', 'build'));
+console.log(p1);
+app.use(p1);
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(express.static('client-side/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client-side', 'build', 'index.html'));
-  });
-}
+app.get('*', (req, res) => {
+  const p2 = path.join(__dirname, 'client-side', 'build', 'index.html');
+  console.log(p2);
+  res.sendFile(p2);
+});
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
